054bb581b0b8bc3346fcdf7b91ee6d68a32240db
Giuseppe "denever" Martino <denever@autistici.org>
2008-01-11 09:29:30 +0100

    Added check grid in rhythmexcbox

--- include/rhythmexcbox.hh
+++ include/rhythmexcbox.hh
@@ -29,7 +29,7 @@
 #define RHYTHMEXCBOX_HH
 
 #include "astream.hh"
-
+#include "checkbox.hh"
 
 #include <gtkmm/scale.h>
 #include <gtkmm/label.h>
@@ -74,6 +74,76 @@ namespace gtkmmorsegui
 	Gtk::Button m_btn_next;	
 	Gtk::Button m_btn_start;
 	Gtk::Button m_btn_stop;
+
+	Gtk::HBox m_box_check;
+	
+	Gtk::Table m_tbl_check1;
+	Gtk::Table m_tbl_check2;
+	Gtk::Table m_tbl_check3;
+
+	Gtk::Entry m_txt_check111;
+	Gtk::Entry m_txt_check112;
+	Gtk::Entry m_txt_check113;
+	Gtk::Entry m_txt_check114;
+	Gtk::Entry m_txt_check115;
+	Gtk::Entry m_txt_check121;
+	Gtk::Entry m_txt_check122;
+	Gtk::Entry m_txt_check123;
+	Gtk::Entry m_txt_check124;
+	Gtk::Entry m_txt_check125;
+	Gtk::Entry m_txt_check131;
+	Gtk::Entry m_txt_check132;
+	Gtk::Entry m_txt_check133;
+	Gtk::Entry m_txt_check134;
+	Gtk::Entry m_txt_check135;
+	Gtk::Entry m_txt_check141;
+	Gtk::Entry m_txt_check142;
+	Gtk::Entry m_txt_check143;
+	Gtk::Entry m_txt_check144;
+	Gtk::Entry m_txt_check145;
+
+	
+	Gtk::Entry m_txt_check211;
+	Gtk::Entry m_txt_check212;
+	Gtk::Entry m_txt_check213;
+	Gtk::Entry m_txt_check214;
+	Gtk::Entry m_txt_check215;
+	Gtk::Entry m_txt_check221;
+	Gtk::Entry m_txt_check222;
+	Gtk::Entry m_txt_check223;
+	Gtk::Entry m_txt_check224;
+	Gtk::Entry m_txt_check225;
+	Gtk::Entry m_txt_check231;
+	Gtk::Entry m_txt_check232;
+	Gtk::Entry m_txt_check233;
+	Gtk::Entry m_txt_check234;
+	Gtk::Entry m_txt_check235;
+	Gtk::Entry m_txt_check241;
+	Gtk::Entry m_txt_check242;
+	Gtk::Entry m_txt_check243;
+	Gtk::Entry m_txt_check244;
+	Gtk::Entry m_txt_check245;
+
+	Gtk::Entry m_txt_check311;
+	Gtk::Entry m_txt_check312;
+	Gtk::Entry m_txt_check313;	
+	Gtk::Entry m_txt_check314;
+	Gtk::Entry m_txt_check315;
+	Gtk::Entry m_txt_check321;	
+	Gtk::Entry m_txt_check322;
+	Gtk::Entry m_txt_check323;
+	Gtk::Entry m_txt_check324;
+	Gtk::Entry m_txt_check325;
+	Gtk::Entry m_txt_check331;	
+	Gtk::Entry m_txt_check332;
+	Gtk::Entry m_txt_check333;
+	Gtk::Entry m_txt_check334;
+	Gtk::Entry m_txt_check335;
+	Gtk::Entry m_txt_check341;	
+	Gtk::Entry m_txt_check342;
+	Gtk::Entry m_txt_check343;
+	Gtk::Entry m_txt_check344;
+	Gtk::Entry m_txt_check345;
 	
 	sigc::signal<void, unsigned int> m_started;
 	sigc::signal<void, std::list<std::string> > m_finished;
@@ -84,4 +154,3 @@ namespace gtkmmorsegui
 }
 
 #endif
-
--- src/rhythmexcbox.cc
+++ src/rhythmexcbox.cc
@@ -33,12 +33,15 @@ using namespace gtkmmorsegui;
 
 RhythmExcBox::RhythmExcBox(Glib::RefPtr<Gnome::Conf::Client> conf_client):
     m_conf_client(conf_client),
-    m_frm_msg("Koch exercise"),
+    m_frm_msg("Rhythm exercise"),
     m_lbl_msg(rhythmexc_explanation),
     m_btn_repeat("repeat"),
     m_btn_next("next"),
     m_btn_start("start"),
-    m_btn_stop("stop")
+    m_btn_stop("stop"),
+    m_tbl_check1(4,5),
+    m_tbl_check2(4,5),
+    m_tbl_check3(4,5)   
 {
     m_frm_msg.add(m_lbl_msg);
 
@@ -59,6 +62,158 @@ RhythmExcBox::RhythmExcBox(Glib::RefPtr<Gnome::Conf::Client> conf_client):
     pack_start(m_hbb_buttons);
     
     m_btn_start.signal_clicked().connect( sigc::mem_fun(*this, &RhythmExcBox::on_btn_start_clicked) );
+    
+    m_txt_check111.set_width_chars(1);
+    m_txt_check112.set_width_chars(1);
+    m_txt_check113.set_width_chars(1);
+    m_txt_check114.set_width_chars(1);
+    m_txt_check115.set_width_chars(1);
+
+    m_txt_check121.set_width_chars(1);
+    m_txt_check122.set_width_chars(1);
+    m_txt_check123.set_width_chars(1);
+    m_txt_check124.set_width_chars(1);
+    m_txt_check125.set_width_chars(1);
+
+    m_txt_check131.set_width_chars(1);
+    m_txt_check132.set_width_chars(1);
+    m_txt_check133.set_width_chars(1);	
+    m_txt_check134.set_width_chars(1);
+    m_txt_check135.set_width_chars(1);
+
+    m_txt_check141.set_width_chars(1);
+    m_txt_check142.set_width_chars(1);
+    m_txt_check143.set_width_chars(1);	
+    m_txt_check144.set_width_chars(1);
+    m_txt_check145.set_width_chars(1);
+
+    m_txt_check211.set_width_chars(1);
+    m_txt_check212.set_width_chars(1);
+    m_txt_check213.set_width_chars(1);
+    m_txt_check214.set_width_chars(1);
+    m_txt_check215.set_width_chars(1);
+
+    m_txt_check221.set_width_chars(1);
+    m_txt_check222.set_width_chars(1);
+    m_txt_check223.set_width_chars(1);
+    m_txt_check224.set_width_chars(1);
+    m_txt_check225.set_width_chars(1);
+
+    m_txt_check231.set_width_chars(1);	
+    m_txt_check232.set_width_chars(1);
+    m_txt_check233.set_width_chars(1);
+    m_txt_check234.set_width_chars(1);
+    m_txt_check235.set_width_chars(1);
+
+    m_txt_check241.set_width_chars(1);	
+    m_txt_check242.set_width_chars(1);
+    m_txt_check243.set_width_chars(1);
+    m_txt_check244.set_width_chars(1);
+    m_txt_check245.set_width_chars(1);
+
+    m_txt_check311.set_width_chars(1);
+    m_txt_check312.set_width_chars(1);
+    m_txt_check313.set_width_chars(1);
+    m_txt_check314.set_width_chars(1);
+    m_txt_check315.set_width_chars(1);
+
+    m_txt_check321.set_width_chars(1);
+    m_txt_check322.set_width_chars(1);
+    m_txt_check323.set_width_chars(1);
+    m_txt_check324.set_width_chars(1);
+    m_txt_check325.set_width_chars(1);
+
+    m_txt_check331.set_width_chars(1);	
+    m_txt_check332.set_width_chars(1);
+    m_txt_check333.set_width_chars(1);
+    m_txt_check334.set_width_chars(1);
+    m_txt_check335.set_width_chars(1);
+
+    m_txt_check341.set_width_chars(1);	
+    m_txt_check342.set_width_chars(1);
+    m_txt_check343.set_width_chars(1);
+    m_txt_check344.set_width_chars(1);
+    m_txt_check345.set_width_chars(1);
+
+    m_tbl_check1.attach(m_txt_check111, 0, 1, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check112, 1, 2, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check113, 2, 3, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check114, 3, 4, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check115, 4, 5, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+
+    m_tbl_check1.attach(m_txt_check121, 0, 1, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check122, 1, 2, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check123, 2, 3, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check124, 3, 4, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check125, 4, 5, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+    
+    m_tbl_check1.attach(m_txt_check131, 0, 1, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check132, 1, 2, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check133, 2, 3, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check134, 3, 4, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check135, 4, 5, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+
+    m_tbl_check1.attach(m_txt_check141, 0, 1, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check142, 1, 2, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check143, 2, 3, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check144, 3, 4, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check1.attach(m_txt_check145, 4, 5, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+
+    m_box_check.pack_start(m_tbl_check1);
+    
+    m_tbl_check2.attach(m_txt_check211, 0, 1, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check212, 1, 2, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check213, 2, 3, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check214, 3, 4, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check215, 4, 5, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+
+    m_tbl_check2.attach(m_txt_check221, 0, 1, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check222, 1, 2, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check223, 2, 3, 1, 2, Gtk::SHRINK, Gtk::SHRINK);	
+    m_tbl_check2.attach(m_txt_check224, 3, 4, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check225, 4, 5, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+
+    m_tbl_check2.attach(m_txt_check231, 0, 1, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check232, 1, 2, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check233, 2, 3, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check234, 3, 4, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check235, 4, 5, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+
+    m_tbl_check2.attach(m_txt_check241, 0, 1, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check242, 1, 2, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check243, 2, 3, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check244, 3, 4, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check2.attach(m_txt_check245, 4, 5, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+
+    m_box_check.pack_start(m_tbl_check2);
+
+    m_tbl_check3.attach(m_txt_check311, 0, 1, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check312, 1, 2, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check313, 2, 3, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check314, 3, 4, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check315, 4, 5, 0, 1, Gtk::SHRINK, Gtk::SHRINK);
+
+    m_tbl_check3.attach(m_txt_check321, 0, 1, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check322, 1, 2, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check323, 2, 3, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check324, 3, 4, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check325, 4, 5, 1, 2, Gtk::SHRINK, Gtk::SHRINK);
+
+    m_tbl_check3.attach(m_txt_check331, 0, 1, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check332, 1, 2, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check333, 2, 3, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check334, 3, 4, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check335, 4, 5, 2, 3, Gtk::SHRINK, Gtk::SHRINK);
+
+    m_tbl_check3.attach(m_txt_check341, 0, 1, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check342, 1, 2, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check343, 2, 3, 3, 4, Gtk::SHRINK, Gtk::SHRINK);	
+    m_tbl_check3.attach(m_txt_check344, 3, 4, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+    m_tbl_check3.attach(m_txt_check345, 4, 5, 3, 4, Gtk::SHRINK, Gtk::SHRINK);
+
+    m_box_check.pack_start(m_tbl_check3);
+
+    pack_start(m_box_check);
 }
 
 RhythmExcBox::~RhythmExcBox()
