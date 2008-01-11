07e037d0099cfd191ebb38b4f710fb9a49fc737d
Giuseppe "denever" Martino <denever@autistici.org>
2008-01-11 18:36:39 +0100

    Fixed include

--- src/radiobuttons.cc
+++ src/radiobuttons.cc
@@ -16,7 +16,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-#include "radiobuttons.h"
+#include "radiobuttons.hh"
+
+using namespace gtkmmorsegui;
 
 RadioButtons::RadioButtons() :
   m_rbt_firstpattern("@"),
