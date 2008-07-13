6311da255e60f14c4dda3bfa0d384d1dc3630023
Giuseppe "denever" Martino <denever@autistici.org>
2008-07-13 14:25:11 +0200

    Renamed Classic Method in Analitic Method

--- include/resources.hh
+++ include/resources.hh
@@ -55,7 +55,7 @@
 #define mainwindow_item3  "Main Exercise"
 #define mainwindow_item4  "Setup"
 #define mainwindow_item5  "Koch Setup"
-#define mainwindow_item6  "Classic method"
+#define mainwindow_item6  "Analitic method"
 
 #define msg_start      "About to start keying. Get ready..."
 #define msg_changes    "Change these settings from the Setup Menu."
